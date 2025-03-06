56f519ade21321dc7c959718b253c6c1818b83ee
ADIMY <abdoadimy@gmail.com>
2025-03-07 00:59:58 +0200

    update a connection of db

--- Config/dataBase.js
+++ Config/dataBase.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 import { v2 as cloudinary } from 'cloudinary';
 
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
-});
 
 const dbConnect = async () => {
+    cloudinary.config({
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.API_KEY,
+        api_secret: process.env.API_SECRET,
+    });
     try {
         const db = process.env.DATA_BASE.replace('<db_password>', process.env.DB_PASSWORD);
         await mongoose.connect(db);
