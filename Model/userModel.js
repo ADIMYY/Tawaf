b86b7c336795ad35bd629f70d002ad2d2c6ae7ab
ADIMY <abdoadimy@gmail.com>
2025-03-08 03:10:00 +0200

    fixing

--- Model/userModel.js
+++ Model/userModel.js
@@ -106,7 +106,7 @@ const userSchema = new mongoose.Schema(
         },
         location: {
             type: String,
-            required: "",
+            default: "",
         }
     }, { timestamps: true }
 );
