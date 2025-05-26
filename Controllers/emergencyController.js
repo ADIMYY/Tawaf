e7c3bb69cd4fcae9607cccb79faa2cb3641ff79f
ADIMY <abdoadimy@gmail.com>
2025-05-26 13:49:47 +0300

    add Around You Feature

--- Controllers/emergencyController.js
+++ Controllers/emergencyController.js
@@ -2,15 +2,6 @@ import asyncHandler from "express-async-handler";
 import Emergency from "../Model/emergencyModel.js";
 import appError from "../Utils/appError.js";
 
-
-export const createEmergency = asyncHandler(async (req, res, next) => {
-    const { city, emergency } = req.body;
-
-    const newEmergency = await Emergency.create({ city, emergency });
-
-    res.status(201).json({ status: 'success', data: newEmergency });
-});
-
 export const getAllEmergencies = asyncHandler(async (req, res, next) => {
     const query = req.query.city ? { city: req.query.city } : {};
     const emergencies = await Emergency.find(query);
@@ -20,28 +11,4 @@ export const getAllEmergencies = asyncHandler(async (req, res, next) => {
     }
 
     res.status(200).json({ status: 'success', data: emergencies });
-});
-
-export const updateEmergency = asyncHandler(async (req, res, next) => {
-    const { city, emergency } = req.body;
-
-    const updatedEmergency = await Emergency.findOneAndUpdate({ city }, { emergency }, { new: true });
-
-    if (!updatedEmergency) {
-        return next(new appError('No emergencies found for this city', 404));
-    }
-
-    res.status(200).json({ status: 'success', data: updatedEmergency });
-});
-
-export const deleteEmergency = asyncHandler(async (req, res, next) => {
-    const { city } = req.params;
-
-    const emergency = await Emergency.findOneAndDelete({ city });
-
-    if (!emergency) {
-        return next(new appError('No emergencies found for this city', 404));
-    }
-
-    res.status(204).json({ status: 'success', data: null });
 });
