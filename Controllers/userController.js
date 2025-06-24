b567e59fc37331f4c0cdd2db0d14bb38e6141d7f
ADIMY <abdoadimy@gmail.com>
2025-06-24 23:26:14 +0300

    update delete user

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -75,7 +75,8 @@ export const updateUser = asyncHandler(async (req, res, next) => {
 //* Delete a user by ID
 export const deleteUser = asyncHandler(async (req, res, next) => {
     // Find the user first to get their image URLs
-    const user = await User.findById(req.params.id);
+    const id = req.params.id || req.user._id; //* Use logged-in user ID if no ID is provided
+    const user = await User.findById(id);
     
     // Check if user existed
     if (!user) {
@@ -121,7 +122,7 @@ export const deleteUser = asyncHandler(async (req, res, next) => {
     }
 
     // Delete the user from database
-    await User.findByIdAndDelete(req.params.id);
+    await User.findByIdAndDelete(id);
 
     // Check if the requesting user has admin privileges
     if (req.user.role === 'admin') {
