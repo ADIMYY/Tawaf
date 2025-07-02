e952891b87ae3e9c00c0b5c1f4facf26c05fcbc6
ADIMY <abdoadimy@gmail.com>
2025-07-03 00:16:30 +0300

    feat: add getDataByName controller and localization for user data

--- Controllers/getDataController.js
+++ Controllers/getDataController.js
@@ -1,3 +1,4 @@
+import slugify from "slugify";
 import asyncHandler from "express-async-handler";
 import User from '../Model/userModel.js';
 import appError from "../Utils/appError.js";
@@ -55,6 +56,26 @@ export const getDataById = asyncHandler(async (req, res, next) => {
 
     const id = req.params.id || req.user._id; // Use logged-in user ID if no ID is provided
     const user = await User.findById(id).select(userFields);
+
+    if (!user) {
+        return next(new appError(t('error.noUserWithId'), 404));
+    }
+
+    const localizedUser = localizeUser(user, t);
+
+    res.status(200).json({ status: 'success', data: localizedUser });
+});
+
+
+export const getDataByName = asyncHandler(async (req, res, next) => {
+    // Extract language from query parameter (e.g., ?lng=en or ?lng=ar)
+    const lang = req.query.lng || 'en'; // Default to English if no language is provided
+    req.i18n.changeLanguage(lang); // Change language based on query parameter
+    const t = req.t; // i18next translation function
+
+    const { name } = req.body;
+
+    const user = await User.findOne({ slug: slugify(name) }).select(userFields);
 
     if (!user) {
         return next(new appError(t('error.noUserWithId'), 404));
--- Routes/getDataRoute.js
+++ Routes/getDataRoute.js
@@ -4,6 +4,7 @@ import {
     getDataFromQrcode,
     getUserByPassport ,
     getDataById,
+    getDataByName,
 } from '../Controllers/getDataController.js';
 
 const router = express.Router();
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get('/Qrcode', getDataFromQrcode);
 router.get('/passport/:passport', getUserByPassport);
 router.get('/id/:id', getDataById);
+router.post('/name', getDataByName);
 
 export default router;
